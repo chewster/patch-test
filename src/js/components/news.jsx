d5bf82bb203b79d4140e575d595b9db0bfbb2969
chewster <cmchiu.chiu@gmail.com>
2020-05-13 12:35:36 -0400

    adding search functionality to the news

--- src/js/components/news.jsx
+++ src/js/components/news.jsx
@@ -16,18 +16,18 @@ export default class News extends Component {
         this.state = {
             articles: [],
             error: null,
-            isLoading: false
+            isLoading: false, 
+            todaysDate: getCurrentDate(),
+            searchWord: ''
         };
+        this.fetchData = this.fetchData.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    componentDidMount() {
+    fetchData(word) {
         this.setState({ isLoading: true });
-        let todaysDate = getCurrentDate(),
-          searchWord = "today",
-          //NEWS_API = 'http://newsapi.org/v2/everything?q='+ searchWord +'&from=' + todaysDate +'&sortBy=publishedAt&apiKey=f9b70e839a1b47e2a8b11a8ad054ec62'
-         NEWS_API = "http://newsapi.org/v2/everything?q=covid&from=2020-05-12&sortBy=publishedAt&apiKey=f9b70e839a1b47e2a8b11a8ad054ec62"; //this is only for latenight testing  
-        console.log("testing the mounting"); 
-        console.log(NEWS_API); 
+        let NEWS_API = 'http://newsapi.org/v2/everything?q=' + word + '&from=' + this.state.todaysDate + '&sortBy=publishedAt&apiKey=f9b70e839a1b47e2a8b11a8ad054ec62';
         fetch(NEWS_API)
             .then(response => {
                 if (response.ok) {
@@ -38,9 +38,24 @@ export default class News extends Component {
             })
             .then(data => this.setState({ articles: data.articles, isLoading: false }))
             .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    handleSubmit(event)  {
+        event.preventDefault();
+        this.fetchData(this.state.searchWord);
+    };
+    handleChange(event) {
+        this.setState({ searchWord: event.target.value });
+    }
+    componentDidMount() {
+        let search = "today";
+        if (this.state.searchWord != null && this.state.searchWord != '') {
+            search = this.state.searchWord;
+        }
+        this.fetchData(search);
     }    
     render() {
-        const { articles, isLoading, error } = this.state;
+        const { articles, isLoading, error, todaysDate, searchWord } = this.state;
         if (error) {
             return <p>{error.message}</p>;
         }
@@ -51,7 +66,7 @@ export default class News extends Component {
         else if (articles == undefined || articles.length== 0) {
             return (
                 <section>
-                    <h2>News of Today</h2> 
+                    <h2>News of Today {todaysDate} </h2> 
                     NO ARTICLES FOUND
                 </section>
             )
@@ -59,7 +74,20 @@ export default class News extends Component {
         else {
             return (
                 <section> 
-                    <h2>News of Today</h2> 
+                    <h2>News of Today {todaysDate} </h2> 
+                    <form className="form-inline" onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            className="form-control mb-2 mr-sm-2 mb-sm-0"
+                            placeholder="Search"
+                            value = {searchWord}
+                            onChange={this.handleChange}
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-primary">submit
+                        </button>
+                    </form>
                     <Carousel className = "news-carousel" itemsToShow={3}>
                         {articles.map(article =>
                         <div className="article">
@@ -67,8 +95,6 @@ export default class News extends Component {
                             <img src={article.urlToImage}></img>
                             <span className="description">{article.description}</span>
                         </div>
-
-    
                         )}
                     </Carousel>
                 </section>
